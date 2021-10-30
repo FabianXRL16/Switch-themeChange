847a4b755d957ce14e9b65ef895ba886576a0a1d
Fabian Pacherres <fabianpacherres@gmail.com>
2021-10-29 20:29:53 -0500

    Change colors

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let $btn = document.querySelector("#btnSwitch");
+let $body = document.querySelector(".body");
+let $title = document.querySelector(".title");
+let $containerSwitch = document.querySelector(".containerSwitch");
+let $btnSwitch = document.querySelector(".btnSwitch");
 let ligth = true;
+
+let primary = "#ff0000";
+let secondary = "#fff";
+
+function colors() {
+  $body.style.backgroundColor = primary;
+  $btnSwitch.style.backgroundColor = secondary;
+  $containerSwitch.style.border = `2px solid ${secondary}`;
+  $title.style.color = secondary;
+}
+
+colors()
 
 function changeTheme() {
   if (ligth) {
@@ -7,12 +23,20 @@ function changeTheme() {
     setTimeout(function () {
       $btn.style.transform = "translateX(30px)";
       $btn.style.width = "10px";
+      $body.style.backgroundColor = secondary;
+      $btnSwitch.style.backgroundColor = primary;
+      $containerSwitch.style.border = `2px solid ${primary}`;
+      $title.style.color = primary;
     }, 300);
   } else {
     $btn.style.transform = "translateX(0px)";
     $btn.style.width = "40px";
     setTimeout(function () {
       $btn.style.width = "10px";
+      $body.style.backgroundColor = primary;
+      $btnSwitch.style.backgroundColor = secondary;
+      $containerSwitch.style.border = `2px solid ${secondary}`;
+      $title.style.color = secondary;
     }, 300);
   }
   ligth = !ligth;
